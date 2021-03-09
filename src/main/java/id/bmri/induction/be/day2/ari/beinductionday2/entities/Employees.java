6159eaa90e58f27a6d2f1a7f8737a3e40cfe838c
Ari Anugrah Arifin <C010321006@Supporting.corp.bankmandiri.co.id>
2021-03-09 09:50:23 +0700

    update native query

---
+++ src/main/java/id/bmri/induction/be/day2/ari/beinductionday2/entities/Employees.java
@@ -0,0 +1,35 @@
+//package id.bmri.induction.be.day2.ari.beinductionday2.entities;
+//
+//
+//import lombok.Data;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Data
+//@Entity
+//@Table(name = "employees")
+//public class Employees {
+//
+//    @Id
+//    @Column(name = "employee_id", nullable = false)
+//    private int employeeId;
+//
+//    @Column(name = "first_name", length = 20)
+//    private String firstName;
+//
+//    @Column(name = "last_name", length = 25, nullable = false)
+//    private String lastName;
+//
+//    @Column(name = "email", length = 25, nullable = false)
+//    private String email;
+//
+//    @Column(name = "phone_number", length = 20)
+//    private String phoneNumber;
+//
+//    @Column(name = "hire_date", nullable = false)
+//    private String
+//
+//}
---
+++ src/main/java/id/bmri/induction/be/day2/ari/beinductionday2/repository/EmployeesRepository.java
@@ -0,0 +1,9 @@
+package id.bmri.induction.be.day2.ari.beinductionday2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeesRepository extends JpaRepository {
+
+}
